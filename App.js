123c7f8de7c6e17df867bff922581467199801a5
codexpression-nj <n.codexpression@gmail.com>
2023-02-26 22:23:01 +0200

    new daily update

--- App.js
+++ App.js
@@ -5,15 +5,21 @@ import SignIn from './components/SignIn';
 import EventList from './components/EventList';
 import Signup from './components/SignUp';
 import NoEventsFound from './components/NoEventsFound';
+import Landing from './components/Landing';
+import ForgotPassword from './components/ForgotPassword';
+import Categories from './components/Categories';
 
 export default function App() {
   return (
   
         <NavigationContainer>
      <Stack.Navigator>
+        <Stack.Screen name="Landing" component={Landing} />
         <Stack.Screen name="List" component={EventList} />
+        <Stack.Screen name="Categories" component={Categories} />
         <Stack.Screen name="SignIn" component={SignIn} />
         <Stack.Screen name="SignUp" component={Signup} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
         <Stack.Screen name="NoEvents" component={NoEventsFound} />
       </Stack.Navigator>
         </NavigationContainer>
